ff6465883de812a84f6be5e78e166a9ceb71c907
Guillaume Friloux <guillaume@friloux.me>
2026-04-17 00:28:30 +0200

    refactor(exampleHost): less and less indirections
    less and less indirections
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- systems/x86_64-linux/exampleHost/boot.nix
+++ systems/x86_64-linux/exampleHost/boot.nix
@@ -4,19 +4,10 @@
   pkgs,
   ...
 }: let
-  cfg = config.zfs-root.boot;
-  inherit (lib) types mkOption mkDefault concatMapStrings;
+  bootDevices = ["nvme-Samsung_SSD_980_PRO_1TB_S5GXNL0W810368E" "nvme-Samsung_SSD_980_PRO_1TB_S5GXNL0W810454N"];
+  availableKernelModules = ["xhci_pci" "ehci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "amdgpu"];
+  inherit (lib) mkDefault concatMapStrings;
 in {
-  options.zfs-root.boot = {
-    bootDevices = mkOption {
-      description = "Specify boot devices";
-      type = types.nonEmptyListOf types.str;
-    };
-    availableKernelModules = mkOption {
-      type = types.nonEmptyListOf types.str;
-      default = ["uas" "nvme" "ahci"];
-    };
-  };
   config = {
     zfs-root.fileSystems = {
       datasets = {
@@ -26,12 +17,12 @@ in {
         "rpool/nixos/root" = "/";
         "bpool/nixos/root" = "/boot";
       };
-      efiSystemPartitions = map (diskName: diskName + "-part1") cfg.bootDevices;
-      swapPartitions = map (diskName: diskName + "-part4") cfg.bootDevices;
+      efiSystemPartitions = map (diskName: diskName + "-part1") bootDevices;
+      swapPartitions = map (diskName: diskName + "-part4") bootDevices;
     };
     boot = {
       kernelPackages = pkgs.linuxPackages_6_12;
-      initrd.availableKernelModules = cfg.availableKernelModules;
+      initrd.availableKernelModules = availableKernelModules;
       kernel.sysctl = {
         "kernel.unprivileged_userns_clone" = 1; # for appimages
         "fs.file-max" = 640000;
@@ -41,12 +32,12 @@ in {
       loader = {
         efi = {
           canTouchEfiVariables = false;
-          efiSysMountPoint = "/boot/efis/" + (builtins.head cfg.bootDevices) + "-part1";
+          efiSysMountPoint = "/boot/efis/" + (builtins.head bootDevices) + "-part1";
         };
         generationsDir.copyKernels = true;
         grub = {
           enable = true;
-          devices = map (diskName: "/dev/disk/by-id/" + diskName) cfg.bootDevices;
+          devices = map (diskName: "/dev/disk/by-id/" + diskName) bootDevices;
           efiInstallAsRemovable = true;
           copyKernels = true;
           efiSupport = true;
@@ -55,7 +46,7 @@ in {
             set -x
             ${pkgs.coreutils-full}/bin/cp -r ${config.boot.loader.efi.efiSysMountPoint}/EFI /boot/efis/${diskName}-part1
             set +x
-          '') (builtins.tail cfg.bootDevices);
+          '') (builtins.tail bootDevices);
         };
       };
     };
