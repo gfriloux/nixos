18942462363960c398bd8b52ccaa3411b601801f
Guillaume Friloux <guillaume@friloux.me>
2026-04-16 09:10:17 +0200

    feat(clochette): better traefik configuration handling
    better traefik configuration handling
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- systems/x86_64-linux/clochette/docker-traefik.nix
+++ systems/x86_64-linux/clochette/docker-traefik.nix
@@ -1,12 +1,72 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  traefikConfig = pkgs.writeText "traefik.yml" ''
+    entryPoints:
+      web:
+        address: ":80"
+        http:
+          redirections:
+            entryPoint:
+              to: websecure
+              scheme: https
+
+      websecure:
+        address: ":443"
+
+    api:
+      dashboard: true
+
+    accessLog:
+      filePath: "/logs/traefik.log"
+      format: json
+      fields:
+        headers:
+          defaultMode: drop
+          names:
+            User-Agent: keep
+            X-Real-Ip: keep
+            X-Forwarded-For: keep
+            X-Forwarded-Proto: keep
+
+    certificatesResolvers:
+      lets-encrypt:
+        acme:
+          email: "guillaume+letsencrypt@friloux.me"
+          storage: "acme.json"
+          tlsChallenge: {}
+
+    providers:
+      docker:
+        watch: true
+        network: web
+      file:
+        filename: "/etc/traefik/traefik_dynamic.yml"
+
+    experimental:
+      plugins:
+        bouncer:
+          moduleName: "github.com/maxlerebourg/crowdsec-bouncer-traefik-plugin"
+          version: "v1.5.1"
+  '';
+in {
+  sops = {
+    secrets = {
+      "services/traefik/conf/traefik_dynamic.yml" = {
+        owner = "root";
+        group = "root";
+        mode = "0400";
+        path = "/srv/docker/traefik/conf/traefik_dynamic.yml";
+      };
+    };
+  };
+
   virtualisation.oci-containers.containers."traefik" = {
     image = "traefik:v3.6.13"; # renovate
     serviceName = "traefik";
 
     volumes = [
       "/run/docker.sock:/var/run/docker.sock"
-      "/srv/docker/traefik/traefik.toml:/traefik.toml"
-      "/srv/docker/traefik/traefik_dynamic.toml:/traefik_dynamic.toml"
+      "/srv/docker/traefik/conf/traefik.yml:/etc/traefik/traefik.yml"
+      "/srv/docker/traefik/conf/traefik_dynamic.yml:/etc/traefik/traefik_dynamic.yml"
       "/srv/docker/traefik/acme.json:/acme.json"
       "/srv/docker/traefik/logs:/logs"
     ];
@@ -39,17 +99,9 @@
         '';
         partOf = ["docker-compose-traefik-root.target"];
         wantedBy = [
-          "docker-compose-traefik-root.target"
           "traefik.service"
         ];
       };
-    };
-
-    targets."docker-compose-traefik-root" = {
-      unitConfig = {
-        Description = "One target to manage traefik";
-      };
-      wantedBy = ["multi-user.target"];
     };
   };
 
@@ -57,5 +109,8 @@
 
   systemd.tmpfiles.rules = [
     "d /srv/docker/traefik/logs 0750 0 0 -"
+    "d /srv/docker/traefik/conf 0750 0 0 -"
+    "d /srv/docker/traefik/acme.json 0600 0 0 -"
+    "C /srv/docker/traefik/conf/traefik.yml 0640 root root ${traefikConfig}"
   ];
 }
