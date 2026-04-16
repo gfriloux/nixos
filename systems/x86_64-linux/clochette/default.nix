24b9cbe804ef89381b031791259e75958bb7f976
Guillaume Friloux <guillaume@friloux.me>
2026-04-16 22:45:18 +0200

    style(clochette): put users config aside
    put users config aside
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- systems/x86_64-linux/clochette/default.nix
+++ systems/x86_64-linux/clochette/default.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   inputs,
-  config,
   ...
 }: {
   imports = [
@@ -14,6 +13,7 @@
     ./backup.nix
     ./notify.nix
     ./network.nix
+    ./users.nix
   ];
 
   sops = {
@@ -60,39 +60,6 @@
       autoPrune.enable = true;
     };
     oci-containers.backend = "docker";
-  };
-
-  users = {
-    mutableUsers = false;
-    users.guillaume = {
-      createHome = true;
-      isNormalUser = true;
-      linger = true;
-      home = "/home/guillaume";
-      description = "Moi";
-      extraGroups = ["wheel" "docker"];
-      useDefaultShell = true;
-      hashedPasswordFile = config.sops.secrets."users/guillaume/hashed-password".path;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETPEOCEETy3EHFswjsoEsMmu4i7TUPCXwPrhVsjH8rE guillaume+perso@friloux.me"
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFE9OazubAILNGPXMxVPBK4vgFVNth2G67JWN3wnB4+tAAAADXNzaDpjbG9jaGV0dGU= clochette@friloux.me"
-      ];
-      shell = pkgs.fish;
-    };
-
-    users.weechat = {
-      createHome = true;
-      isNormalUser = true;
-      linger = true;
-      home = "/home/weechat";
-      description = "Moi";
-      useDefaultShell = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETPEOCEETy3EHFswjsoEsMmu4i7TUPCXwPrhVsjH8rE guillaume+perso@friloux.me"
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIP69OQvGEPoEZU8pSCRKDprle3C9UGqbt/52t6NG5GWYAAAADnNzaDphcHB3ZWVjaGF0 weechat@irc.friloux.me"
-      ];
-      shell = pkgs.fish;
-    };
   };
 
   programs.fish.enable = true;
