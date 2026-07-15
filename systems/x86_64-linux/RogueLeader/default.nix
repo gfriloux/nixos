a8d043691f8e6aa84691caaa49ac99fe511d3b6b
Guillaume Friloux <guillaume@friloux.me>
2026-07-15 10:25:16 +0200

    feat(RogueLeader): use docker-proxy
    use docker-proxy
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- systems/x86_64-linux/RogueLeader/default.nix
+++ systems/x86_64-linux/RogueLeader/default.nix
@@ -8,6 +8,7 @@
     inputs.sops-nix.nixosModules.sops
     inputs.disko.nixosModules.disko
     inputs.stc.nixosModules.relics-docker-traefik
+    inputs.stc.nixosModules.relics-docker-socket-proxy
     inputs.stc.nixosModules.relics-docker-crowdsec
     inputs.stc.nixosModules.relics-docker-notify
     ./hardware-configuration.nix
@@ -28,6 +29,10 @@
       acme.email = "guillaume+letsencrypt@friloux.me";
       enableDashboard = true;
       dynamicConfigFile = config.sops.secrets."services/traefik/conf/traefik_dynamic.yml".path;
+    };
+    socketProxy = {
+      enable = true;
+      image = "tecnativa/docker-socket-proxy:0.3.0"; # renovate
     };
     crowdsec = {
       enable = true;
